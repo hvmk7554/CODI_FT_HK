cb1f1141ebb30b8c742491247a55f0f14a697d79
hvmk7554 <57095401+hvmk7554@users.noreply.github.com>
2023-09-08 19:19:44 +0700

    Add files via upload

---
+++ buoi8/js/script.js
@@ -0,0 +1,75 @@
+const productnames= document.querySelectorAll('.name');
+const productprices= document.querySelectorAll('.price');
+const buttons= document.querySelectorAll('.addtocartbtn');
+if(localStorage.getItem('cart') && localStorage.getItem('cart')!= null)
+{ var result = localStorage.getItem('cart');
+var arr = JSON.parse(result);
+
+}
+else {
+var arr=[];}
+fetch();
+buttons.forEach((el,index) => {  //nhan vao +1
+    el.addEventListener('click',()=>{
+        var item= new Object();
+        item.id=index;
+        item.name=productnames[index].innerText;
+        item.price=productprices[index].innerText;
+        item.qty=1
+        var check = false;
+        arr.forEach(el1 => {
+            if(el1.id==index){
+                el1.qty++;
+                check=true;
+            }
+        });
+        if(check==false){
+            arr.push(item);
+        }
+        // console.log(arr);
+        localStorage.setItem('cart',JSON.stringify(arr));
+        
+        fetch ()        
+    })
+});
+function fetch(){   //nam trong button.forEach
+    if(arr.length>0){
+        var str=``;
+        var i = 1;
+        arr.forEach((el,index) => {
+            str+=`
+            <tr class="">
+                <td scope="row">`+(i++)+`</td>
+                <td>`+el.name+`</td>
+                <td>`+Intl.NumberFormat('en-US').format(el.price)+`</td>
+                <td>`+el.qty+`</td>
+                <td>`+Intl.NumberFormat('en-US').format((el.qty*el.price))+`</td>
+                <td><button class="btn btn-danger btn-sm" onclick="deleteCart(`+index+`)" >Xóa</button></td>
+            </tr>
+            `
+        });
+        document.getElementById('resultcart').innerHTML=str;
+    }else{
+        str=`
+        <tr class="">
+        <td colspan=6 scope="row">Chưa có giỏ hàng</td>
+        </tr>
+        `;
+        document.getElementById('resultcart').innerHTML=str;
+    }
+}
+
+function deleteCart(x){  //nut xoa, nam trong fetch
+    if(arr.length>1){
+        arr = arr.filter(item => item.id!=x);
+    }else{
+        arr=[];
+    }
+    if(arr.length==0){
+        localStorage.removeItem('cart');
+    }else{
+        localStorage.setItem('cart',JSON.stringify(arr));
+    }
+            
+    window.location.reload
+}
