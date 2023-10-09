14b4e52ac199ecfd8d5607b369c1ae173fd1213f
hvmk7554 <57095401+hvmk7554@users.noreply.github.com>
2023-10-09 18:58:37 +0700

    Add files via upload

---
+++ buoi29/src/pages/Cart/Cart.jsx
@@ -0,0 +1,75 @@
+/* eslint-disable no-unused-vars */
+import Pagelayout from "../../components/Pagelayout";
+import CartItem from "./CartItem";
+import { useSelector } from "react-redux";
+import { useDispatch} from "react-redux";
+import { decrease, increase, removeCart } from "../../rtk/slices/cartSlice";
+
+function Cart() {
+const {cartList} = useSelector(state => state.cart);
+const dispatch = useDispatch();
+
+const handleIncrease = (item) => {
+dispatch(increase(item))
+}
+
+const handleDecrease = (item) => {
+if (item.quantity <= 1) { 
+  dispatch(removeCart(item))
+} else {
+dispatch(decrease(item))
+}
+}
+
+const handleRemove = (item) => { 
+  dispatch(removeCart(item))
+}
+
+const totalPrice = cartList.reduce((acc, curr) => acc + curr.price * curr.quantity,0) 
+console.log(totalPrice)
+
+
+return ( <Pagelayout>
+
+      <div className="h-auto bg-[#FBFBF8] pt-20">
+        
+        <h1 className="mb-18 text-center text-2x1 font-bold">Cart Items</h1>
+        <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 x1:px-0"> 
+        <div className="rounded-lg md:w-2/3">
+          {cartList.length > 0 && cartList.map((item) => (
+            <CartItem key={item.id} item = {item} 
+            handleRemove={() => handleRemove(item)}
+            handleIncrease={() => handleIncrease(item)}
+            handleDecrease={() => handleDecrease(item)}
+            />
+  ))}
+          {cartList.length == 0 && (<img src="../src/assets/empty_cart.jfif" alt="not-found" />)}
+        </div>
+          <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
+            <div className="mb-2 flex justify-between">
+              <p className="text-gray-700">Subtotal</p>
+              <p className="text-gray-700">${totalPrice}</p>
+            </div>
+            <div className="flex justify-between">
+              <p className="text-gray-700">Shipping</p>
+              <p className="text-gray-700">$0</p>
+            </div>
+            <hr className="my-4" />
+            <div className="flex justify-between">
+              <p className="text-lg font-bold">Total</p>
+              <div className>
+                <p className="mb-1 text-lg font-bold">${totalPrice}</p>
+                <p className="text-sm text-gray-700">including VAT</p>
+              </div>
+            </div>
+            <button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">
+              Check out
+            </button>
+          </div>
+        </div>
+      </div>
+    </Pagelayout>
+  );
+}
+
+export default Cart;
---
+++ buoi29/src/rtk/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit"; 
+import todolistReducer from "./slices/todolistSlice";
+import authReducer from "./slices/authSlice";
+import cartReducer from "./slices/cartSlice";
+
+export const store = configureStore({
+reducer : {
+todolist: todolistReducer,
+auth: authReducer,
+cart: cartReducer
+}
+})
